69ddd59d4dbe830fc17ad0da2a5548357011aef7
Luis Enrique Sanchez Romero <luiskike2210@gmail.com>
2025-11-25 09:48:06 -0600

    Test 1

--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "quick-sos-front-7brg-git-main-luisenrique22xds-projects.vercel.app",
 ]
 
 # Application definition
